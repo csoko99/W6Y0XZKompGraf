c9ef95233afa38770f2739bf159873e2680f3da7
csoko99 <laczkolali77@gmail.com>
2024-05-09 16:55:37 +0200

    2.feladat kész

--- W6Y0XZ_0425/GUIW6Y0XZ/src/hu/the/w6y0xz/W6Y0XZ_2D_Grafika.java
+++ W6Y0XZ_0425/GUIW6Y0XZ/src/hu/the/w6y0xz/W6Y0XZ_2D_Grafika.java
@@ -16,31 +16,36 @@ public class W6Y0XZ_2D_Grafika extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 Graphics2D g2d = (Graphics2D) g;
-
+                
                 setBackground(Color.WHITE);
-
+                
+                Shape hiddenellipse = new Arc2D.Double(-100, 0, 510, 400, 90, 360, Arc2D.OPEN);
+                
                 g2d.rotate(Math.toRadians(-45), 100, 150);
                 Arc2D ellipse = new Arc2D.Double(-100, 200, 400, 200, 90, 360/2, Arc2D.OPEN);
-                g2d.setColor(Color.BLUE);
+                g2d.setPaint(new GradientPaint(200, 200, Color.BLUE, 210, 210, Color.YELLOW, true ));
                 g2d.fill(ellipse);
-
-                Arc2D ellipse2 = new Arc2D.Double(200, 350, 200, 200, 30, 120, Arc2D.OPEN); 
-                g2d.setColor(Color.GREEN);
+                g2d.setClip(hiddenellipse);
+                
+                Shape ellipse2 = new Ellipse2D.Float(100, 300, 400, 700);
+                g2d.setPaint(Color.GREEN);
                 g2d.fill(ellipse2);
+                g2d.setPaint(Color.MAGENTA);
+                g2d.setStroke(new BasicStroke(5));
+                g2d.draw(ellipse2);
+                g2d.setClip(hiddenellipse);
 
+                g2d.setPaint(new GradientPaint(30, 30, Color.BLACK, 40, 40, Color.RED, true));
+                g2d.setFont(new Font("Arial", Font.BOLD, 26));
+                g2d.drawString("Tokaj-Hegyalja Egyetem", 30, 350);
+                
                 ImageIcon imageIcon = new ImageIcon("egyetem.png");
                 Image image = imageIcon.getImage();
                 int width = imageIcon.getIconWidth() / 4;
                 int height = imageIcon.getIconHeight() / 4;
-
                 g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.8f));
-
-                g2d.setColor(Color.RED);
-                g2d.setFont(new Font("Arial", Font.BOLD, 26));
-                g2d.drawString("Tokaj-Hegyalja Egyetem", 30, 350);
-
-           
                 g2d.drawImage(image, 90, 100, width, height, this);
+                g2d.setClip(90, 100, 300, 400);
             }
         };
 
