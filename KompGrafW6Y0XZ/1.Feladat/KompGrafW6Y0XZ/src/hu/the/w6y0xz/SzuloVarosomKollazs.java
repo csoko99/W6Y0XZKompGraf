ad8552c0243751d81f619ac930f965a6f31fc94b
csoko99 <72137613+csoko99@users.noreply.github.com>
2024-05-11 17:04:03 +0200

    little semantic fix

--- KompGrafW6Y0XZ/1.Feladat/KompGrafW6Y0XZ/src/hu/the/w6y0xz/SzuloVarosomKollazs.java
+++ KompGrafW6Y0XZ/1.Feladat/KompGrafW6Y0XZ/src/hu/the/w6y0xz/SzuloVarosomKollazs.java
@@ -41,59 +41,59 @@ public class SzuloVarosomKollazs extends JFrame
 	            Shape hiddenellipse = new Arc2D.Double(350, 250, 300, 300, 90, 360, Arc2D.OPEN);
 	            AffineTransform old = g2d.getTransform();
 	            
-	            g2d.setFont(new Font("Times New Roman", Font.BOLD, 26));
-                g2d.drawString("Appoló mozi", 540, 40);
+	            	g2d.setFont(new Font("Times New Roman", Font.BOLD, 26));
+                	g2d.drawString("Appoló mozi", 540, 40);
 	        	g2d.setColor(Color.BLACK);
 	        	Image mozi = apollo.getImage();
 	        	g2d.drawImage (mozi, 500, 50, width, height, this);
 	        	
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 26));
-                g2d.drawString("Déri múzeum", 540, 590);
+                	g2d.drawString("Déri múzeum", 540, 590);
 	        	Image muzeum = deri.getImage();
 	        	g2d.drawImage (muzeum, 500, 600, width, height, this);
 	        	
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 26));
-                g2d.drawString("Debreceni Egyetem", 210, 590);
+                	g2d.drawString("Debreceni Egyetem", 210, 590);
 	        	Image egyetem_kep = egyetem.getImage();
 	        	g2d.drawImage (egyetem_kep, 200, 600, width, height, this);
 	        	
 	        	g2d.rotate(Math.toRadians(90), 560, 400);
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 20));
-                g2d.drawString("Modem", 450, 0);
-                g2d.setTransform(old);
+               		g2d.drawString("Modem", 450, 0);
+                	g2d.setTransform(old);
 	        	Image modem_kep = modem.getImage();
 	        	g2d.drawImage (modem_kep, 700, 250, width, height, this);
 	        	
 	        	g2d.rotate(Math.toRadians(90), 560, 400);
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 20));
-                g2d.drawString("Nagyerdő", 610, 0);
-                g2d.setTransform(old);
+                	g2d.drawString("Nagyerdő", 610, 0);
+                	g2d.setTransform(old);
 	        	Image nagyerdo_kep = nagyerdo.getImage();
 	        	g2d.drawImage (nagyerdo_kep, 700, 420, width, height, this);
 	        	
 	        	g2d.rotate(Math.toRadians(270), 305, 305);
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 20));
-                g2d.drawString("LOKI stadion", 60, 40);
-                g2d.setTransform(old);
+                	g2d.drawString("LOKI stadion", 60, 40);
+                	g2d.setTransform(old);
 	        	Image stadion_kep = stadion.getImage();
 	        	g2d.drawImage (stadion_kep, 50, 420, width, height, this);
 	        	
 	        	
-                g2d.rotate(Math.toRadians(270), 305, 305);
+                	g2d.rotate(Math.toRadians(270), 305, 305);
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 20));
-                g2d.drawString("Nagyerdei strand", 210, 40);
-                g2d.setTransform(old);
+                	g2d.drawString("Nagyerdei strand", 210, 40);
+                	g2d.setTransform(old);
 	        	Image strand_kep = strand.getImage();
 	        	g2d.drawImage (strand_kep, 50, 250, width, height, this);
 	        	
 	        	g2d.setFont(new Font("Times New Roman", Font.BOLD, 26));
-                g2d.drawString("Nagyerdei víztorony", 210, 40);
+                	g2d.drawString("Nagyerdei víztorony", 210, 40);
 	        	Image torony = viztorony.getImage();
 	        	g2d.drawImage (torony, 200, 50, width, height, this);
 	        	
 	        	g2d.setPaint(new GradientPaint(30, 30, Color.BLUE, 40, 40, Color.BLACK, true));
-                g2d.setFont(new Font("Arial", Font.BOLD, 36));
-                g2d.drawString("Szülővárosom - Debrecen", 300, 240);
+                	g2d.setFont(new Font("Arial", Font.BOLD, 36));
+                	g2d.drawString("Szülővárosom - Debrecen", 300, 240);
                 
 	        	g2d.setClip(hiddenellipse);
 	        	Image templom = nagytemplom.getImage();
